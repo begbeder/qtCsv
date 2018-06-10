cf0aec510a27cd907b0884a425daaf752617b304
Begbeder <safonov.front@gmail.com>
2018-06-10 20:11:15 +0700

    Добавлена поддержка русского языка

--- qtProject/main.cpp
+++ qtProject/main.cpp
@@ -1,8 +1,10 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8")); // Добавлена поддержка русского языка при работе с файлами
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
